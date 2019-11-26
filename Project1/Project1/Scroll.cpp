8fd321c88f081bc7ddadee3fce0cbadf3223d914
GA3 <GA3@DEV1-11>
2019-11-26 14:21:29 +0900

    a

--- Project1/Project1/Scroll.cpp
+++ Project1/Project1/Scroll.cpp
@@ -19,8 +19,8 @@ CObjScroll::CObjScroll(int map[46][100])
 //イニシャライズ
 void CObjScroll::Init()
 {
-	m_scroll = 64.0f*-90.0f;
-	l_scroll = 64.0f*-15.0f;
+	m_scroll = 64.0f*0.0f;
+	l_scroll = 64.0f*-35.0f;
 }
 //アクション
 void CObjScroll::Action()
