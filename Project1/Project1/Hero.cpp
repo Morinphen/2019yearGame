3845e363b78fbf50a538db92e3bb531080dd24e3
GA3 <GA3@DEV1-11>
2019-12-05 11:16:34 +0900

    土遁更新

--- Project1/Project1/Hero.cpp
+++ Project1/Project1/Hero.cpp
@@ -258,6 +258,8 @@ void CObjHero::Action()
 						ball = true;
 					}
 				}
+				//忍術モード解除で土遁解除
+				doton = false;
 			}
 
 			//妖術モード
@@ -305,7 +307,6 @@ void CObjHero::Action()
 
 		}
 	}
-
 	//うちかぎを使用しているとき
 	else if (U_flag == true)
 	{
