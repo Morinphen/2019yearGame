db324ea5a5516b908dcc83677133d05efb57f841
GA3 <GA3@DEV1-11>
2019-12-05 09:58:15 +0900

    調整

--- Project1/Project1/Otosiana.cpp
+++ Project1/Project1/Otosiana.cpp
@@ -139,9 +139,10 @@ void CObjOtosiana::Draw()
 	RECT_F src;
 	RECT_F dst;
 
-	// 表示画面内の時
-		CObjScroll* scroll = (CObjScroll*)Objs::GetObj(OBJ_SCROLL);
-	if (scroll->Inscrooll_check(m_x, m_y) == true)
+	// 表示画面内の時&土遁中消える
+	CObjScroll* scroll = (CObjScroll*)Objs::GetObj(OBJ_SCROLL);
+	CObjHero* hero = (CObjHero*)Objs::GetObj(OBJ_HERO);
+	if (scroll->Inscrooll_check(m_x, m_y) == true || hero->GetDoton() == false)
 	{
 
 		//ブロック表示
