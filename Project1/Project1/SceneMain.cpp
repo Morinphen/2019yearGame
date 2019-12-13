04926f93347467313efd2996ff5e3cd39b839664
GA3 <GA3@DEV1-5>
2019-12-13 15:42:41 +0900

    浦ＭＡＰ3号じゃ

--- Project1/Project1/SceneMain.cpp
+++ Project1/Project1/SceneMain.cpp
@@ -104,7 +104,7 @@ void CSceneMain::InitScene()
 	//外部データの読み込み（ステージ情報）
 	unique_ptr<wchar_t> p;  //ステージ情報のポインター
 	int size;               //ステージ情報の大きさ
-	p = Save::ExternalDataOpen(L"NIN.csv", &size);//外部データ読み込み
+	p = Save::ExternalDataOpen(L"NIN2.csv", &size);//外部データ読み込み
 
 	//unique_ptr<wchar_t> p2;  //ステージ情報のポインター
 	//int size2;               //ステージ情報の大きさ
